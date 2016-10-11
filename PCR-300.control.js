a978ef347151584bb2b6a9741f8c966feb939851
Andreas Linz <klingt.net@gmail.com>
2016-10-11 17:46:29 +0200

    Fix port names

--- PCR-300.control.js
+++ PCR-300.control.js
@@ -3,4 +3,4 @@ loadAPI(1);
 host.defineController("Edirol", "PCR-300", "1.0", "08B54374-2AF7-4C1F-AB81-E4DDA0957165");
 host.defineMidiPorts(3, 2);
 
-host.addDeviceNameBasedDiscoveryPair(["PCR Midi", "PCR 1", "PCR 2"], ["PCR Midi", "PCR 2"]);
+host.addDeviceNameBasedDiscoveryPair(["PCR MIDI", "PCR 1", "PCR 2"], ["PCR MIDI", "PCR 1"]);
