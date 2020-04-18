099de0794b29cd03817b6f36f8fc9fe249b085c9
Andreas Linz <alinz@fastmail.com>
2020-04-18 13:10:09 +0200

    Merge multi maven module into single module
    This is a lot easier to maintain, install and so on.

---
+++ src/main/java/net/klingt/akai/midimix/MidiHandler.java
@@ -0,0 +1,249 @@
+package net.klingt.akai.midimix;
+
+import com.bitwig.extension.api.util.midi.ShortMidiMessage;
+import com.bitwig.extension.callback.ShortMidiDataReceivedCallback;
+import com.bitwig.extension.controller.api.CursorRemoteControlsPage;
+import com.bitwig.extension.controller.api.MasterTrack;
+import com.bitwig.extension.controller.api.RemoteControl;
+import com.bitwig.extension.controller.api.Track;
+import com.bitwig.extension.controller.api.TrackBank;
+import com.bitwig.extension.controller.api.Transport;
+import com.bitwig.extension.controller.api.UserControlBank;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static net.klingt.akai.midimix.MidiMix.BANK_LEFT;
+import static net.klingt.akai.midimix.MidiMix.BANK_RIGHT;
+import static net.klingt.akai.midimix.MidiMix.FADERS;
+import static net.klingt.akai.midimix.MidiMix.KNOBS;
+import static net.klingt.akai.midimix.MidiMix.KNOBS_USER;
+import static net.klingt.akai.midimix.MidiMix.KNOBS_TOP_ROW;
+import static net.klingt.akai.midimix.MidiMix.MASTER_FADER;
+import static net.klingt.akai.midimix.MidiMix.MIDI_RESOLUTION;
+import static net.klingt.akai.midimix.MidiMix.MUTE;
+import static net.klingt.akai.midimix.MidiMix.REC_ARM;
+import static net.klingt.akai.midimix.MidiMix.SOLO;
+import static net.klingt.akai.midimix.MidiMix.SOLO_MODE;
+import static net.klingt.akai.midimix.MidiMix.indexOf;
+import static net.klingt.akai.midimix.MidiMix.isNotIn;
+
+public class MidiHandler implements ShortMidiDataReceivedCallback {
+    private final Transport transport;
+    private final MasterTrack masterTrack;
+    private final TrackBank trackBank;
+    private final Map<Integer, Consumer<ShortMidiMessage>> noteHandlers;
+    private final Map<Integer, Consumer<ShortMidiMessage>> ccHandlers;
+    private final CursorRemoteControlsPage cursorRemoteControlsPage;
+    private final UserControlBank userControls;
+    private boolean soloMode;
+
+    MidiHandler(Transport transport, MasterTrack masterTrack, TrackBank trackBank, CursorRemoteControlsPage cursorRemoteControlsPage, UserControlBank userControls) {
+        this.transport = transport;
+        this.masterTrack = masterTrack;
+        this.trackBank = trackBank;
+        this.noteHandlers = registerNoteHandlers();
+        this.ccHandlers = registerCCHandlers();
+        this.cursorRemoteControlsPage = cursorRemoteControlsPage;
+        this.userControls = userControls;
+    }
+
+    private Map<Integer, Consumer<ShortMidiMessage>> registerCCHandlers() {
+        if (this.ccHandlers != null) {
+            return Collections.emptyMap();
+        }
+
+        Map<Integer, Consumer<ShortMidiMessage>> ccHandlers = new HashMap<>();
+        ccHandlers.put(MASTER_FADER, this::handleMasterFader);
+        Arrays.stream(FADERS).forEach(key -> ccHandlers.put(key, this::handleFader));
+        Arrays.stream(KNOBS).forEach(key -> ccHandlers.put(key, this::handleKnob));
+        return ccHandlers;
+    }
+
+    private void handleKnob(ShortMidiMessage msg) {
+        if (!msg.isControlChange()) {
+            return;
+        }
+
+        Optional<Integer> topRowIdx = indexOf(msg.getData1(), KNOBS_TOP_ROW);
+        if (topRowIdx.isPresent()) {
+            topRowIdx.ifPresent(idx -> handleTopRow(idx, msg.getData2()));
+        } else {
+            handleUserControl(msg);
+        }
+    }
+
+    private void handleUserControl(ShortMidiMessage msg) {
+        indexOf(msg.getData1(), KNOBS_USER)
+                .map(idx -> userControls.getControl(idx))
+                .ifPresent(param -> param.value().set(msg.getData2(), MIDI_RESOLUTION));
+    }
+
+    private void handleTopRow(Integer idx, Integer value) {
+        if (idx >= cursorRemoteControlsPage.getParameterCount()) {
+            return;
+        }
+        RemoteControl parameter = cursorRemoteControlsPage.getParameter(idx);
+        parameter.set(value, MIDI_RESOLUTION);
+    }
+
+    private void handleFader(ShortMidiMessage msg) {
+        if (!msg.isControlChange()) {
+            return;
+        }
+
+        indexOf(msg.getData1(), FADERS)
+                .filter(i -> i < trackBank.getSizeOfBank())
+                .map(trackBank::getChannel)
+                .ifPresent(ch -> ch.getVolume().set(msg.getData2(), MIDI_RESOLUTION));
+    }
+
+    private void handleMasterFader(ShortMidiMessage msg) {
+        if (!msg.isControlChange()) {
+            return;
+        }
+
+        masterTrack.getVolume().set(msg.getData2(), MIDI_RESOLUTION);
+    }
+
+    private Map<Integer, Consumer<ShortMidiMessage>> registerNoteHandlers() {
+        if (this.noteHandlers != null) {
+            return Collections.emptyMap();
+        }
+
+        Map<Integer, Consumer<ShortMidiMessage>> noteHandlers = new HashMap<>();
+        noteHandlers.put(SOLO_MODE, this::handleSoloMode);
+        noteHandlers.put(BANK_LEFT, this::handleBankLeftRight);
+        noteHandlers.put(BANK_RIGHT, this::handleBankLeftRight);
+        Arrays.stream(REC_ARM).forEach(key -> noteHandlers.put(key, this::handleArm));
+        Arrays.stream(SOLO).forEach(key -> noteHandlers.put(key, this::handleSolo));
+        Arrays.stream(MUTE).forEach(key -> noteHandlers.put(key, this::handleMute));
+        return noteHandlers;
+    }
+
+    @Override
+    public void midiReceived(int statusByte, int data1, int data2) {
+        ShortMidiMessage msg = new ShortMidiMessage(statusByte, data1, data2);
+        handleCC(msg);
+        handleNote(msg);
+    }
+
+    /**
+     * handleCC calls registered handlers, if any, on an incoming MIDI control change.
+     *
+     * @param msg MIDI event
+     */
+    private void handleCC(ShortMidiMessage msg) {
+        if (!msg.isControlChange()) {
+            return;
+        }
+        if (!ccHandlers.containsKey(msg.getData1())) {
+            return;
+        }
+
+        ccHandlers.get(msg.getData1()).accept(msg);
+    }
+
+    /**
+     * handleNote calls registered handlers, if any, on an incoming MIDI note event.
+     *
+     * @param msg MIDI event
+     */
+    private void handleNote(ShortMidiMessage msg) {
+        if (!msg.isNoteOn() && !msg.isNoteOff()) {
+            return;
+        }
+        if (!noteHandlers.containsKey(msg.getData1())) {
+            return;
+        }
+
+        noteHandlers.get(msg.getData1()).accept(msg);
+    }
+
+    private void handleSoloMode(ShortMidiMessage msg) {
+        soloMode = msg.isNoteOn();
+    }
+
+    private void handleBankLeftRight(ShortMidiMessage msg) {
+        if (!msg.isNoteOn()) {
+            return;
+        }
+
+        switch (msg.getData1()) {
+            case BANK_LEFT:
+                trackBank.scrollPageBackwards();
+                return;
+            case BANK_RIGHT:
+                trackBank.scrollPageForwards();
+        }
+    }
+
+    private void handleSolo(ShortMidiMessage msg) {
+        if (!msg.isNoteOn() || isNotIn(msg.getData1(), SOLO)) {
+            return;
+        }
+
+        indexOf(msg.getData1(), SOLO)
+                .filter(i -> i < trackBank.getSizeOfBank())
+                .map(trackBank::getChannel)
+                .ifPresent(this::toggleSolo);
+    }
+
+    private void toggleSolo(Track track) {
+        track.getSolo().toggle();
+    }
+
+    private void handleMute(ShortMidiMessage msg) {
+        if (!msg.isNoteOn() || isNotIn(msg.getData1(), MUTE)) {
+            return;
+        }
+
+        indexOf(msg.getData1(), MUTE)
+                .filter(i -> i < trackBank.getSizeOfBank())
+                .map(trackBank::getChannel)
+                .ifPresent(this::toggleMute);
+    }
+
+    private void toggleMute(Track track) {
+        track.getMute().toggle();
+    }
+
+    private void handleArm(ShortMidiMessage msg) {
+        if (!msg.isNoteOn() || isNotIn(msg.getData1(), REC_ARM)) {
+            return;
+        }
+
+        indexOf(msg.getData1(), REC_ARM)
+                .filter(i -> i < trackBank.getSizeOfBank())
+                .map(trackBank::getChannel)
+                .ifPresent(this::toggleArm);
+    }
+
+    private void toggleArm(Track track) {
+        track.getArm().toggle();
+    }
+
+    void sysexReceived(final String data) {
+        // MMC Transport Controls:
+        switch (data) {
+            case "f07f7f0605f7":
+                transport.rewind();
+                break;
+            case "f07f7f0604f7":
+                transport.fastForward();
+                break;
+            case "f07f7f0601f7":
+                transport.stop();
+                break;
+            case "f07f7f0602f7":
+                transport.play();
+                break;
+            case "f07f7f0606f7":
+                transport.record();
+        }
+    }
+}
---
+++ src/main/java/net/klingt/edirol/pcr/EdirolPCRExtensionDefinition.java
@@ -0,0 +1,114 @@
+package net.klingt.edirol.pcr;
+
+import com.bitwig.extension.api.PlatformType;
+import com.bitwig.extension.controller.AutoDetectionMidiPortNamesList;
+import com.bitwig.extension.controller.ControllerExtensionDefinition;
+import com.bitwig.extension.controller.api.ControllerHost;
+
+import java.util.UUID;
+
+import static java.lang.String.format;
+
+public class EdirolPCRExtensionDefinition extends ControllerExtensionDefinition {
+    private static final UUID DRIVER_ID = UUID.fromString("253deb05-198c-443c-bec7-5fa0f44da207");
+
+    public EdirolPCRExtensionDefinition() {
+    }
+
+    @Override
+    public String getName() {
+        return "Edirol PCR";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "klingt.net";
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.2.1";
+    }
+
+    @Override
+    public UUID getId() {
+        return DRIVER_ID;
+    }
+
+    @Override
+    public String getHardwareVendor() {
+        return "Edirol";
+    }
+
+    @Override
+    public String getHardwareModel() {
+        return "Edirol PCR";
+    }
+
+    @Override
+    public int getRequiredAPIVersion() {
+        return 4;
+    }
+
+    @Override
+    public int getNumMidiInPorts() {
+        return 3;
+    }
+
+    @Override
+    public int getNumMidiOutPorts() {
+        return 2;
+    }
+
+    @Override
+    public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list,
+                                               final PlatformType platformType) {
+        switch (platformType) {
+            case WINDOWS:
+                System.err.println(format("Support for platform '%s' is experimental.", platformType.name()));
+            case MAC:
+                list.add(new String[]{"PCR MIDI IN", "PCR 1", "PCR 2"}, new String[]{"PCR MIDI OUT", "PCR"});
+                break;
+            case LINUX:
+                list.add(new String[]{"PCR MIDI", "PCR 1", "PCR 2"}, new String[]{"PCR MIDI", "PCR 1"});
+        }
+    }
+
+    @Override
+    public EdirolPCRExtension createInstance(final ControllerHost host) {
+        return new EdirolPCRExtension(this, host);
+    }
+
+    public enum MidiInPorts {
+        EXTERNAL(0),
+        // keyboard nad left most controls
+        ONE(1),
+        // top control row (knobs, faders, pads, etc.)
+        TWO(2);
+
+        private final int value;
+
+        MidiInPorts(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public enum MidiOutPorts {
+        EXTERNAL(0),
+        ONE(1);
+
+        private final int value;
+
+        MidiOutPorts(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+}
